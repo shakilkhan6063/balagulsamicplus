02885fbd79bfd45f5eb75ff85b811d5b6241f5af
shakilkhan6063 <122600220+shakilkhan6063@users.noreply.github.com>
2023-01-16 00:36:22 +0600

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a,b;
+float mul;
+ cout<<"Enter the number of a: ";
+ cin>>a;
+ cout<<"Enter the number of b: ";
+ cin>>b;
+ cout<<"Enter the result :";
+ mul=a+b;
+cout<<"mul= "<<mul;
+ return 0;
+
+}
